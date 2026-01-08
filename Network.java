d9cf8a0b512008e53e8835056821bd6cec95ed96
Apple <Apple@MacBook-Air.local>
2026-01-08 23:13:12 +0200

    new

--- Network.java
+++ Network.java
@@ -61,12 +61,16 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (this.getUser(name1) == null | this.getUser(name2) == null | this.getUser(name1).follows(name2) | name1.equals(name2)) {
+        if (this.getUser(name1) == null | this.getUser(name2) == null | name1.equals(name2)) {
+            return false;
+        }
+        
+        if (this.getUser(name1).follows(name2)) {
             return false;
         }
 
-        this.getUser(name1).addFollowee(name2);
-        return true;
+        boolean result = this.getUser(name1).addFollowee(name2);
+        return result;
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
